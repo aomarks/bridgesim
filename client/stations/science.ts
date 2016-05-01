258987244f8fb41c520a006635a8b38bfa1a596b
Tristan Rice <rice@fn.lc>
2016-05-01 16:47:02 -0700

    Updated science station

--- client/stations/science.ts
+++ client/stations/science.ts
@@ -1,6 +1,10 @@
+///<reference path="../../bower_components/polymer-ts/polymer-ts.d.ts" />
+///<reference path="../../core/ship.ts" />
+
 namespace Bridgesim.Client.Stations {
   @component('science-station')
   class Science extends polymer.Base {
+    @property({type: Array, value: []}) ships: Core.Ship[];
     draw() {
 
     }
