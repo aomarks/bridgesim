d3dae181552d81d61f3e82ae8c876b68ef36cc45
Alexander Marks <aomarks@gmail.com>
2016-05-23 23:18:52 -0700

    Draw all entity types in new full screen map.

--- client/map2.ts
+++ client/map2.ts
@@ -2,12 +2,17 @@
 
 import * as color from './colors';
 import {Db} from '../core/entity/db';
+import {HP} from './const';
 import {lerp, snap} from './util';
+import {radians} from '../core/util';
+import {Position} from '../core/components';
 
 interface Coord2D {
   x: number;
   y: number;
 }
+
+const BLIP_PX = 2;
 
 @component('bridgesim-map2')
 export class Map2 extends polymer.Base {
@@ -22,6 +27,7 @@ export class Map2 extends polymer.Base {
   private can: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
   private shipImage: HTMLImageElement;
+  private stationImage: HTMLImageElement;
   private w: number;  // canvas width
   private h: number;  // canvas height
   private screenCenter: Coord2D = {x: 0, y: 0};
@@ -33,6 +39,8 @@ export class Map2 extends polymer.Base {
     this.ctx = this.can.getContext('2d');
     this.shipImage = new Image();
     this.shipImage.src = '/images/ship.svg';
+    this.stationImage = new Image();
+    this.stationImage.src = '/images/station.svg';
     window.addEventListener('resize', this.resize.bind(this));
   }
 
@@ -51,6 +59,21 @@ export class Map2 extends polymer.Base {
           (y * this.zoom) - this.panY
     };
   }
+
+  lerpScreenPos(id: string, alpha: number): Position {
+    const pos = this.db.positions[id];
+    if (pos == null) {
+      return null;
+    }
+    let prev = this.db.prevPositions[id];
+    if (prev == null) {
+      prev = pos;
+    }
+    const s = this.worldToScreen(
+        lerp(pos.x, prev.x, alpha), lerp(pos.y, prev.y, alpha));
+    return {x: s.x, y: s.y, yaw: lerp(pos.yaw, prev.yaw, alpha), roll: 0};
+  }
+
 
   draw(localAlpha: number, remoteAlpha: number): void {
     if (!this.drawn) {
@@ -73,7 +96,12 @@ export class Map2 extends polymer.Base {
     }
 
     this.drawGrid();
-    this.drawShip(localAlpha);
+    this.drawDebris(remoteAlpha);
+    this.drawStations(remoteAlpha);
+    this.drawRemoteShips(remoteAlpha);
+    this.drawLasers(remoteAlpha);
+    this.drawMissiles(remoteAlpha);
+    this.drawLocalShip(localAlpha);
   }
 
   drawGrid(): void {
@@ -92,19 +120,114 @@ export class Map2 extends polymer.Base {
     ctx.stroke();
   }
 
-  drawShip(alpha: number): void {
+  drawDebris(alpha: number): void {
+    for (let id in this.db.debris) {
+      const coords = this.lerpScreenPos(id, alpha);
+      if (coords == null) {
+        continue;
+      }
+      this.drawBlip(coords.x, coords.y, '#964B00');
+    }
+  }
+
+  drawStations(alpha: number): void {
+    for (let id in this.db.stations) {
+      const coords = this.lerpScreenPos(id, alpha);
+      if (coords == null) {
+        continue;
+      }
+      this.drawImage(coords.x, coords.y, this.stationImage, 0, 1 / 2);
+      const name = this.db.names[id];
+      this.drawText(coords.x + 10, coords.y + 5, name);
+      const health = this.db.healths[id];
+      if (health != null) {
+        this.drawText(coords.x + 10, coords.y + 20, health.hp.toString());
+      }
+    }
+  }
+
+  drawRemoteShips(alpha: number): void {
+    for (let id in this.db.ships) {
+      if (id === this.shipId) {
+        continue;
+      }
+      const coords = this.lerpScreenPos(id, alpha);
+      if (coords == null) {
+        return;
+      }
+      this.ctx.beginPath();
+      this.drawBlip(coords.x, coords.y, '#FF0000');
+      const name = this.db.names[id];
+      this.drawText(coords.x + 10, coords.y + 5, name);
+      const health = this.db.healths[id];
+      if (health != null) {
+        this.drawText(coords.x + 10, coords.y + 20, health.hp.toString());
+      }
+    }
+  }
+
+  drawLasers(alpha: number): void {
+    const ctx = this.ctx;
+    for (let id in this.db.lasers) {
+      const pos = this.lerpScreenPos(id, alpha);
+      if (pos == null) {
+        continue;
+      }
+      let rads = radians(pos.yaw - 90);
+      ctx.beginPath();
+      ctx.moveTo(pos.x, pos.y);
+      ctx.lineTo(pos.x + Math.cos(rads) * 20, pos.y + Math.sin(rads) * 20);
+      ctx.strokeStyle = '#F00';
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+  }
+
+  drawMissiles(alpha: number): void {
+    const ctx = this.ctx;
+    for (let id in this.db.missiles) {
+      const pos = this.lerpScreenPos(id, alpha);
+      if (pos == null) {
+        continue;
+      }
+      let rads = radians(pos.yaw - 90);
+      ctx.beginPath();
+      ctx.moveTo(pos.x, pos.y);
+      ctx.lineTo(pos.x + Math.cos(rads) * 5, pos.y + Math.sin(rads) * 5);
+      ctx.strokeStyle = color.AQUA;
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+  }
+
+  drawLocalShip(alpha: number): void {
     if (this.shipId == null) {
       return;
     }
-    const pos = this.db.positions[this.shipId];
-    let prev = this.db.prevPositions[this.shipId];
-    if (!prev) {
-      prev = pos;
+    const pos = this.lerpScreenPos(this.shipId, alpha);
+    if (pos == null) {
+      return;
     }
-    const coords = this.worldToScreen(
-        lerp(pos.x, prev.x, alpha), lerp(pos.y, prev.y, alpha));
-    const yaw = lerp(pos.yaw, prev.yaw, alpha);
-    this.drawImage(coords.x, coords.y, this.shipImage, yaw, 1 / 2);
+    this.drawImage(pos.x, pos.y, this.shipImage, pos.yaw, .5);
+  }
+
+  drawText(x: number, y: number, text: string): void {
+    const ctx = this.ctx;
+    ctx.beginPath();
+    ctx.fillStyle = '#FFF';
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 3;
+    ctx.strokeText(text, x, y);
+    ctx.fillText(text, x, y);
+  }
+
+  drawBlip(x: number, y: number, color: string): void {
+    const ctx = this.ctx;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.arc(x, y, BLIP_PX, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
   }
 
   drawImage(
