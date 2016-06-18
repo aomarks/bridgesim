995b4f77bbaf8931d923ed5f82586a38696a2bb2
Tristan Rice <rice@fn.lc>
2016-06-18 17:18:33 -0400

    Switched to using "

--- client/resource-display.ts
+++ client/resource-display.ts
@@ -1,15 +1,14 @@
 ///<reference path="../bower_components/polymer-ts/polymer-ts.d.ts" />
 
-import {Db} from "../core/entity/db";
-import {Resource} from "../core/resources";
+import {Db} from '../core/entity/db';
+import {Resource} from '../core/resources';
 
-@component("resource-display")
+@component('resource-display')
 export class ResourceDisplay extends polymer.Base {
   @property({type: Object}) db: Db;
   @property({type: String}) shipId: string;
 
-  ready(): void {
-  }
+  ready(): void {}
 
   resources(resourcesDb: {[id: string]: {[type: string]: number}}) {
     const stationResources = resourcesDb[this.shipId];
