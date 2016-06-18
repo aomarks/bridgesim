750e9f06b839af47d1c0b807848b425f040abc2c
Tristan Rice <wiz@cockroachlabs.com>
2016-06-17 23:24:40 -0400

    Helm station now zooms with scrollwheel

--- client/stations/helm.ts
+++ client/stations/helm.ts
@@ -1,6 +1,21 @@
-@component('helm-station')
+///<reference path="../../bower_components/polymer-ts/polymer-ts.d.ts" />
+///<reference path="../../typings/index.d.ts" />
+
+@component("helm-station")
 class Helm extends polymer.Base {
   @property({type: Number, notify: true}) zoom: number;
+
+  @listen("wheel")
+  handleZoom(e: any) {
+    let zoom = this.zoom + e.deltaY / 1000;
+    if (zoom < 0) {
+      zoom = 0;
+    } else if (zoom > 1) {
+      zoom = 1;
+    }
+    this.zoom = zoom;
+    e.preventDefault();
+  }
 
   draw(localAlpha: number, remoteAlpha: number) {
     this.$.map.draw(localAlpha, remoteAlpha);
