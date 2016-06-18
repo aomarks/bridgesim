a4bd8cc05fdc24ea16851e183822757cd2e9d905
Tristan Rice <wiz@cockroachlabs.com>
2016-06-18 14:31:59 -0400

    Made lasers use up 10 energy

--- core/entity/laser.ts
+++ core/entity/laser.ts
@@ -1,4 +1,5 @@
 import {Db} from "./db";
+import {Resource} from "../resources";
 
 export function SpawnLaser(
     db: Db, origin: string, x: number, y: number, yaw: number) {
@@ -11,6 +12,7 @@ export function SpawnLaser(
   db.healths[id] = {hp: 0, shields: false};
   db.collidables[id] =
       {length: 10, width: 10, mass: 0, damage: 1, ignore: origin};
+  db.resources[origin][Resource.Energy] -= 10;
   console.log('entity.laser: spawned laser', id);
   return id;
 }
