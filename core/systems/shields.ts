9637fd6e7ab97f1cca88d6b3006f3dd954264a86
Tristan Rice <rice@fn.lc>
2016-08-13 14:13:37 -0500

    shields: use 0.02 energy per tick and power down if out of power

--- core/systems/shields.ts
+++ core/systems/shields.ts
@@ -1,6 +1,8 @@
 import {Db} from '../entity/db';
 
 const DEFAULT_SHIELD_REGEN_RATE = 0.5;
+// Energy consumed by maximum shield regen in one tick.
+const SHIELD_ENERGY = .02;
 
 export class Shields {
   constructor(private db: Db) {}
@@ -8,13 +10,22 @@ export class Shields {
   public tick(): void {
     for (let id in this.db.healths) {
       const health = this.db.healths[id];
+      if (health.shieldsUp) {
+        const res = this.db.resources[id];
+        if (res.energy < SHIELD_ENERGY) {
+          health.shieldsUp = false;
+        } else {
+          res.energy -= SHIELD_ENERGY;
+        }
+      }
+
       if (health.shields >= health.shieldsMax) {
         continue;
       }
+
       const power = this.db.power[id];
-      health.shields = Math.min(
-          health.shields + (power ? power.shields : DEFAULT_SHIELD_REGEN_RATE),
-          health.shieldsMax);
+      const regen_rate = power ? power.shields : DEFAULT_SHIELD_REGEN_RATE;
+      health.shields = Math.min(health.shields + regen_rate, health.shieldsMax);
     }
   }
 }
