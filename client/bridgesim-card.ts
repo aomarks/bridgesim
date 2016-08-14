159cd40e2d4aa03618070946d5201b16e0709f80
Tristan Rice <rice@fn.lc>
2016-08-14 11:24:11 -0500

    refactored the map overlay card style into a reusable component

---
+++ client/bridgesim-card.ts
@@ -0,0 +1,5 @@
+///<reference path="../bower_components/polymer-ts/polymer-ts.d.ts" />
+
+@component('bridgesim-card')
+export class BridgeSimCard extends polymer.Base { }
+BridgeSimCard.register();
