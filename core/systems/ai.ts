93bc44ea4468f81f4ff286a8f75003e157d50a61
Tristan Rice <rice@fn.lc>
2016-08-14 15:01:35 -0500

    ai: AIs now shoot at enemy ships with their weapons

--- core/systems/ai.ts
+++ core/systems/ai.ts
@@ -4,23 +4,94 @@ import {randPoint} from '../galaxy';
 import {dist, heading, headingToRadians} from '../math';
 import {Pathfinder} from '../pathfinding';
 import {every} from '../util';
+import {WeaponType, fireWeapon} from '../weapon';
+
+import {Collision} from './collision';
 
 export class Ai {
   private previousPaths: {[id: string]: Point[]} = {};
   private newPaths: {[id: string]: Point[]} = {};
 
-  constructor(private db: Db, private galaxySize: number) {}
+  constructor(
+      private db: Db, private galaxySize: number,
+      private collision: Collision) {}
 
-  public tick = every(1, () => {
+  public tick() {
+    this.tickMotion();
+    this.tickTargetting();
+  };
+
+  private tickTargetting = every(0.5, () => {
+    for (let id in this.db.ais) {
+      const ai = this.db.ais[id];
+      const {x, y, yaw} = this.db.positions[id];
+      const health = this.db.healths[id];
+      let maxRangeWeapon = 0;
+      for (let weapon of health.weapons) {
+        if (weapon.range > maxRangeWeapon) {
+          maxRangeWeapon = weapon.range;
+        }
+      }
+
+      // Find all ships and stations within the maximum possible range.
+      const nearby = this.collision.quadtree.retrieve(
+          x, y, x + maxRangeWeapon * 2, y + maxRangeWeapon * 2, true);
+      const candidates = nearby.filter((targetID: string): boolean => {
+        if (!(this.db.ships[targetID] || this.db.stations[targetID])) {
+          return false;
+        }
+        return this.hostility(id, targetID) > 1;
+      });
+
+      // Attempt to fire each weapon at nearby targets.
+      const pos = this.db.positions[id];
+      for (let weapon of health.weapons) {
+        // Fire missiles once every 5 seconds.
+        if (weapon.type == WeaponType.Missile && Math.random() > 1 / 10) {
+          continue;
+        }
+
+        for (let cid of candidates) {
+          // Make sure they're in range.
+          if (dist(pos, this.db.positions[cid]) > weapon.range) {
+            continue;
+          }
+
+          const h = heading(this.db.positions[cid], this.db.positions[id]);
+          if (fireWeapon(this.db, id, weapon, h)) {
+            break;
+          }
+        }
+      }
+    }
+  });
+
+  private hostility(a: string, b: string): number {
+    return Math.abs(this.factionScore(a) - this.factionScore(b));
+  }
+
+  private factionScore(id: string): number {
+    const ai = this.db.ais[id];
+    if (ai) {
+      return ai.friendliness;
+    }
+    return 1;
+  }
+
+  // tickMotion will run every second.
+  private tickMotion = every(1, () => {
     this.newPaths = {};
     const finder = new Pathfinder(this.db, this.galaxySize);
     for (let id in this.db.ais) {
-      this.tickOne(id, finder);
+      if (!this.db.motion[id]) {
+        continue;
+      }
+      this.tickMotionOne(id, finder);
     }
     this.previousPaths = this.newPaths;
   });
 
-  tickOne(id: string, finder: Pathfinder): void {
+  private tickMotionOne(id: string, finder: Pathfinder): void {
     const thisPos = this.db.positions[id];
     const ai = this.db.ais[id];
     let targetPos = ai.targetPos;
--- core/weapon.ts
+++ core/weapon.ts
@@ -1,4 +1,8 @@
 import {Point} from './components';
+import {Db} from './entity/db';
+import {SpawnLaser} from './entity/laser';
+import {SpawnMissile} from './entity/missile';
+import {degrees} from './math';
 
 export enum WeaponType {
   Laser = 1,
@@ -17,4 +21,26 @@ export interface FireWeapon {
   heading: number;
   target: Point;
   weapon: Weapon;
+}
+
+// fireWeapon returns true if the weapon was fired. Only won't fire if the
+// weapon can't aim that way or doesn't have any of the required resource.
+export function fireWeapon(
+    db: Db, id: string, w: Weapon, heading: number): boolean {
+  const pos = db.positions[id];
+  const {angle, range, type, damage, direction} = w;
+  const angleDeg = degrees(angle) / 2;
+  const diff = Math.abs(
+      (pos.yaw + degrees(direction) - heading + 180 + 360) % 360 - 180);
+  if (diff > angleDeg) {
+    return false;
+  }
+
+  const resources = db.resources[id];
+  if (type === WeaponType.Laser) {
+    return !!SpawnLaser(db, id, pos.x, pos.y, heading, range, damage);
+  } else if (type == WeaponType.Missile) {
+    return !!SpawnMissile(db, id, pos.x, pos.y, heading, range);
+  }
+  return false;
 }
